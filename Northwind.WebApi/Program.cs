e0958ea7c6558380a9d1f3efb99e73aa6aca36d2
AlexLenitschek <alexander.lenitschek@gmx.de>
2025-04-05 16:25:26 +0200

    Adding part of HybridCaching including Repositories for Customer which we want to cache for better access.

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,8 +1,22 @@
+using Northwind.EntityModels; // To use AddNorthwindContext method.
+using Microsoft.Extensions.Caching.Hybrid; // To use HybridCacheEntryOptions.
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddNorthwindContext();
+
+// Add/Register HybridCache services
+builder.Services.AddHybridCache(options =>
+{
+    options.DefaultEntryOptions = new HybridCacheEntryOptions
+    {
+        Expiration = TimeSpan.FromSeconds(60),
+        LocalCacheExpiration = TimeSpan.FromSeconds(30)
+    };
+});
 
 var app = builder.Build();
 
