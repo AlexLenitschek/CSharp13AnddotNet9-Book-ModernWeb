89cff45b4e67fdfaae920c7d36dcff2fb65eac65
AlexLenitschek <alexander.lenitschek@gmx.de>
2025-04-07 21:02:58 +0200

    Added .http files to also test POST, PUT and DELETE of the WebApi. Also added httplogging and Windows Only Event logging

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,12 +1,14 @@
 using Northwind.EntityModels; // To use AddNorthwindContext method.
 using Microsoft.Extensions.Caching.Hybrid; // To use HybridCacheEntryOptions.
 using Northwind.WebApi.Repositories; // To use ICustomerRepository.
+using Microsoft.AspNetCore.HttpLogging; // To use HttpLoggingFields.
+using System.Runtime.InteropServices; // To use RuntimeInformation
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(documentName: "v2");
 builder.Services.AddNorthwindContext();
 
 // Add/Register HybridCache services
@@ -24,6 +26,20 @@ builder.Services.AddHybridCache(options =>
 // Database context is also registered as a scoped dependency. Therefore cannot register repo as singleton.
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
+// Add logging to the request pipeline.
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+    options.RequestBodyLogLimit = 4096; // 4 KB // Default is 32 KB.
+    options.ResponseBodyLogLimit = 4096; // 4 KB // Default is 32 KB.
+});
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    builder.Logging.AddEventLog();
+}
+// builder.Logging.AddEventLog(); Only supported by Windows and throws warning. Above removes warning.
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +47,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseHttpLogging();
 
 app.UseHttpsRedirection();
 
